13a71709a4f528e7082d1a59afe6891fbbe21954
Miedos <204057201+Miedos@users.noreply.github.com>
2026-05-06 12:18:01 +0200

    FIX fixea seed

--- src/seed.ts
+++ src/seed.ts
@@ -5,10 +5,8 @@ import {
     cuentaAdminPorDefecto,
     efectosPoblacion,
     //getAchievementById,
-    getCardById,
     //getEffectById,
     tablerosPoblacion,
-    getUserByEmail,
     logrosPoblacion,
     mazosPorDefecto
 } from "./poblation/CrearDatosBase.js";
@@ -20,9 +18,9 @@ import { getEffectById } from "./dist/services/Effects.js";
 import { createBoard } from "./dist/services/Boards.js";
 import { createAchievement } from "./dist/services/Achievements.js";
 import { createCosmetic } from "./dist/services/Cosmetics.js";
-import { createCard } from "./dist/services/Cards.js";
+import { createCard, getCardById } from "./dist/services/Cards.js";
 import { createEffect } from "./dist/services/Effects.js";
-import { createUser } from "./dist/services/Users.js";
+import { createUser, getUserByEmail } from "./dist/services/User.js";
 
 async function seedIfMissing<T>(
     label: string,
